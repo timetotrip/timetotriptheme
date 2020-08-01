ca147e8ef42b13f54074e626f6d81e4e9f887e5f
timetotrip <timetotrip420@gmail.com>
2020-08-01 19:58:40 +0900

    CLOUD RAND

--- func-menu.php
+++ func-menu.php
@@ -1,8 +1,53 @@
 <?php
-	function putHeaderMenu(){
-        $ret = "test";
+function putHeaderMenu(){
+    $ret = '';
+    $ret .= '<div id="navimenu">';
+    $ret .=     '<input type="checkbox" id="spmenuon">';
+    $ret .=     '<label for="spmenuon" class="spmenutoggle">';
+    $ret .=     '</label>';
+    $ret .= '</div>';
+    return $ret;
+}
 
-
-        return $ret;
-    }
+function putMenuCate( $cate, $not_in, $num, $order, $dect=false ){
+    $wpq = new WP_Query(array(
+        'category_name' => $cate,
+        'orderby' => $order,
+        'category__not_in' => $not_in,
+        'posts_per_page' => $num
+    ));
+    
+    if ( $wpq->have_posts() ) :
+        while ( $wpq->have_posts() ): $wpq->the_post();
+            
+            
+            if ( has_post_thumbnail() ) : 
+                echo "<a class=\"tpv\" href=\""
+                    . get_permalink()
+                    ."\" >"
+                    . "<img src=\""
+                    . getThumbnailById(get_post_thumbnail_id())
+                    . "\" class=\"tpv-bg\" loading=\"lazy\" alt=\"\">"
+                    . "<div class=\"tpv-area\">";
+                foreach(get_the_category() as $cate):
+                    echo "<p class=\"tpva-categ\">"
+                                .$cate->cat_name 
+                                ."</p>";
+                endforeach;
+                
+                echo "<h2 class=\"tpva-title ptn-str-brown-trans\">"
+                            . get_the_title()
+                        ."</h2>";
+                
+                if ($dect):
+                        echo "<div class=\"tpva-dect\">";
+                            the_excerpt();
+                        echo "</div>";
+                endif;
+                echo "</div></a>";
+            endif;
+        endwhile;
+    endif;
+    
+}
 ?>
