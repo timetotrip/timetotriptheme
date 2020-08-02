94f9cff91c6a51221599736fb77126bd136af560
timetotrip <timetotrip420@gmail.com>
2020-08-03 01:38:14 +0900

    一覧表示

--- func-menu.php
+++ func-menu.php
@@ -4,6 +4,14 @@ function putHeaderMenu(){
     $ret .= '<div id="navimenu">';
     $ret .=     '<input type="checkbox" id="navimenu--on">';
     $ret .=     '<div class="navimenu--area">';
+    $ret .=         '<div class="navimenu--contant">';
+    $ret .=         putH1Index("Site Menu");; 
+    $ret .=         '<p>test</p>'; 
+    $ret .=         putMenuCate('futured', array(),0,'date'); 
+    $ret .=         putMenuCate('cannabisculture',array(5),0,'date'); 
+    $ret .=         putMenuCate("tripnotes",array(5),0,'date'); 
+    $ret .=         putMenuCate('tripguide',array(5),0,'date'); 
+    $ret .=         '</div>'; 
     $ret .=         '<label for="navimenu--on" class="navimenu--toggle tglbutton-menu">';
     $ret .=             '<span class="tglbutton-menu--bar1"></span>';
     $ret .=             '<span class="tglbutton-menu--bar2"></span>';
@@ -14,7 +22,9 @@ function putHeaderMenu(){
     return $ret;
 }
 
-function putMenuCate( $cate, $not_in, $num, $order, $dect=false ){
+function putMenuCate( $cate, $not_in, $num, $order){
+    $ret = '';
+
     $wpq = new WP_Query(array(
         'category_name' => $cate,
         'orderby' => $order,
@@ -23,36 +33,26 @@ function putMenuCate( $cate, $not_in, $num, $order, $dect=false ){
     ));
     
     if ( $wpq->have_posts() ) :
+        $ret .=get_category_by_slug( $cate )->name;
         while ( $wpq->have_posts() ): $wpq->the_post();
             
             
-            if ( has_post_thumbnail() ) : 
-                echo "<a class=\"tpv\" href=\""
-                    . get_permalink()
-                    ."\" >"
-                    . "<img src=\""
-                    . getThumbnailById(get_post_thumbnail_id())
-                    . "\" class=\"tpv-bg\" loading=\"lazy\" alt=\"\">"
-                    . "<div class=\"tpv-area\">";
-                foreach(get_the_category() as $cate):
-                    echo "<p class=\"tpva-categ\">"
-                                .$cate->cat_name 
-                                ."</p>";
-                endforeach;
+            //if ( has_post_thumbnail() ) : 
+                $ret .= '<a class="" href="'. get_permalink().'" >';
+                            //.'<img src="'. getThumbnailById(get_post_thumbnail_id())
+                            //                . '"" loading="lazy" alt="">'
+                            //. '<div class="">';
+               // foreach(get_the_category() as $cate):
+                //    $ret .=         '<p class="">'.$cate->cat_name .'</p>';
+                //endforeach;
                 
-                echo "<h2 class=\"tpva-title ptn-str-brown-trans\">"
-                            . get_the_title()
-                        ."</h2>";
+                $ret .=             '<p class="">'. get_the_title().'</p>';
                 
-                if ($dect):
-                        echo "<div class=\"tpva-dect\">";
-                            the_excerpt();
-                        echo "</div>";
-                endif;
-                echo "</div></a>";
-            endif;
+                //$ret .=        '</div>';
+                $ret .= '</a>';
+            //endif;
         endwhile;
     endif;
-    
+    return $ret;
 }
 ?>
