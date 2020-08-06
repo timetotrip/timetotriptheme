821a518091f0fa1dbcc6b5eb9c9781848c07fa07
timetotrip <timetotrip420@gmail.com>
2020-08-06 23:41:35 +0900

    DOOOONE

--- functions.php
+++ functions.php
@@ -18,54 +18,54 @@ require_once("func-comm.php");
  * Theme Default
  * * * * * * * * * * * * * * * * * */
 if ( ! defined( '_T_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_T_VERSION', '1.0.0' );
+    // Replace the version number of the theme on each release.
+    define( '_T_VERSION', '1.0.0' );
 }
 
 if ( ! function_exists( '_t_setup' ) ) :
-	function _t_setup() {
-		load_theme_textdomain( '_t', get_template_directory() . '/languages' );
-		add_theme_support( 'automatic-feed-links' );
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'post-thumbnails' );
-		register_nav_menus(
-			array(
-				'menu-1' => esc_html__( 'Primary', '_t' ),
-			)
-		);
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			)
-		);
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'_s_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
-		add_theme_support( 'customize-selective-refresh-widgets' );
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
-	}
+    function _t_setup() {
+        load_theme_textdomain( '_t', get_template_directory() . '/languages' );
+        add_theme_support( 'automatic-feed-links' );
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'post-thumbnails' );
+        register_nav_menus(
+            array(
+                'menu-1' => esc_html__( 'Primary', '_t' ),
+            )
+        );
+        add_theme_support(
+            'html5',
+            array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'style',
+                'script',
+            )
+        );
+        add_theme_support(
+            'custom-background',
+            apply_filters(
+                '_s_custom_background_args',
+                array(
+                    'default-color' => 'ffffff',
+                    'default-image' => '',
+                )
+            )
+        );
+        add_theme_support( 'customize-selective-refresh-widgets' );
+        add_theme_support(
+            'custom-logo',
+            array(
+                'height'      => 250,
+                'width'       => 250,
+                'flex-width'  => true,
+                'flex-height' => true,
+            )
+        );
+    }
 endif;
 add_action( 'after_setup_theme', '_t_setup' );
 
@@ -93,14 +93,14 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+    require get_template_directory() . '/inc/jetpack.php';
 }
 
 /**
  * Load WooCommerce compatibility file.
  */
 if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
+    require get_template_directory() . '/inc/woocommerce.php';
 }
 /*remove_action('wp_head', 'adjacent_posts_rel_link_wp_head'); */
 
--- single.php
+++ single.php
@@ -10,61 +10,63 @@
 get_header();
 ?>
 
-	<div id="container">
-		<main id="primary" class="site-main single">
+    <div id="container">
+        <main id="primary" class="site-main single">
 
-			<?php
-			
-			
-			while ( have_posts() ) :
-				the_post();
-				$pid = get_the_ID();
-				setPostViews($pid);
-				
-				echo putH1Index("");
-				
-				echo '<div class="s-firstview sdw_card">'
-							.'<img src="'
-								. getThumbnailById(get_post_thumbnail_id())
-								. '" class="sf-img" loading="lazy" alt="">'
-								. '<div class="sf-title">'
-									.'<h1>' .get_the_title() . '</h1>'
-								. '</div>'
-						.'</div>';
-				
-				breadcrumb(); 
-				
-				echo "<article>";
-					echo TidyContent( do_shortcode( get_the_content() ), array(
-						'-3' => putCate((get_the_category())[0], $pid),
-						'-4' => putSuggest((get_the_category())[0], get_the_tags() ,$pid),
-						'-5' => putFutured(),
-						'-6' => putRand($pid)
-					));
-				echo "</article>";
-				wp_reset_query();
+            <?php
+            
+            
+            while ( have_posts() ) :
+                the_post();
+                $pid = get_the_ID();
+                setPostViews($pid);
+                
+                echo putH1Index("");
+                
+                echo '<div class="s-firstview sdw_card">'
+                            .'<img src="'
+                                . getThumbnailById(get_post_thumbnail_id())
+                                . '" class="sf-img" loading="lazy" alt="">'
+                                . '<div class="sf-title">'
+                                    .'<h1>' .get_the_title() . '</h1>'
+                                . '</div>'
+                        .'</div>';
+                
+                breadcrumb(); 
+                
+                echo "<article>";
+                    echo TidyContent( do_shortcode( get_the_content() ), array(
+                        '-3' => putCate((get_the_category())[0], $pid),
+                        '-4' => putSuggest((get_the_category())[0], get_the_tags() ,$pid),
+                        '-5' => putFutured(),
+                        '-6' => putRand($pid)
+                    ));
+                echo "</article>";
+                wp_reset_query();
 
 
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-				the_post_navigation(
-					array(
-						'prev_text' => 
-							'<i class="fas fa-angle-double-left nav-subtitle ptn-txgrad-fire sdw_card"></i>'
-							.'<p class="nav-title">%title</p>'
-						,
-						'next_text' => 
-						'<i class="fas fa-angle-double-right nav-subtitle ptn-txgrad-fire sdw_card"></i>'
-						.' <p class="nav-title">%title</p>',
-					)
-				);
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
 
-			endwhile; // End of the loop.
-			?>
+                echo putH2Index('次はこれがおススメ');
+                the_post_navigation(
+                    array(
+                        'prev_text' => 
+                            '<i class="fas fa-angle-double-left nav-subtitle ptn-txgrad-fire sdw_card"></i>'
+                            .'<p class="nav-title">%title</p>'
+                        ,
+                        'next_text' => 
+                        '<i class="fas fa-angle-double-right nav-subtitle ptn-txgrad-fire sdw_card"></i>'
+                        .' <p class="nav-title">%title</p>',
+                    )
+                );
 
-		</main><!-- #main -->
-		
+            endwhile; // End of the loop.
+            ?>
+
+        </main><!-- #main -->
+        
 <?php
 get_sidebar();
 get_footer();
